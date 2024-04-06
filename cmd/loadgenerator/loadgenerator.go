65ff67f51a1abb729487ed554f5587de76078852
valentin-carl <vacleantrinl@gmail.com>
2024-04-06 18:22:46 +0200

    add basic load generator

--- cmd/loadgenerator/loadgenerator.go
+++ cmd/loadgenerator/loadgenerator.go
@@ -1,10 +1,61 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"gitcom.com/valentin-carl/f4s/pkg/loadgenerator"
+	"github.com/VividCortex/multitick"
 	"github.com/fatih/color"
 	"log"
+	"sync"
+	"time"
+)
+
+var (
+	dst  = flag.String("dst", "", "target URL")
+	dur  = flag.Duration("t", time.Minute, "experiment duration")
+	nps  = flag.Int("n", 1, "number of parallel streams")
+	size = flag.Int("size", 1024, "message size in bytes")
 )
 
 func main() {
+
+	// TODO
+	//  at the moment, there is a set of workers that send messages in regular intervals
+	//  this might have to be adjusted depending on what will be measured in the experiments (e.g., throughput)
+	// TODO
+	//  adjust what to workers do to desired load pattern/use case
+
 	log.Print(color.GreenString("starting load generator"))
+
+	flag.Parse()
+	log.Printf("load generator input: %s %v %d %d", *dst, *dur, *nps, *size)
+
+	mt := multitick.NewTicker(time.Second, 0)
+	defer mt.Stop()
+
+	ctxAll, cancel := context.WithTimeout(context.Background(), time.Duration(*dur))
+	defer cancel()
+
+	var wg sync.WaitGroup
+	for i := 0; i < *nps; i++ {
+		ctx := context.WithValue(ctxAll, loadgenerator.Key0, struct {
+			id   int
+			seed int64
+			size int
+		}{
+			i,
+			int64(i),
+			*size,
+		})
+		go func() {
+			wg.Add(1)
+			loadgenerator.GenerateLoad(ctx, mt.Subscribe(), dst)
+			wg.Done()
+		}()
+	}
+
+	<-ctxAll.Done()
+	wg.Wait()
+	log.Print(color.GreenString("load generator done"))
 }
--- pkg/loadgenerator/loadgenerator.go
+++ pkg/loadgenerator/loadgenerator.go
@@ -1,1 +1,85 @@
 package loadgenerator
+
+import (
+	"context"
+	"github.com/fatih/color"
+	"github.com/gorilla/websocket"
+	"log"
+	"math/rand"
+	"time"
+)
+
+const (
+	a = 97
+	z = 122
+)
+
+type Message []byte
+
+type LGContextKey int
+
+const (
+	Key0 LGContextKey = iota
+)
+
+func GenerateLoad(ctx context.Context, t <-chan time.Time, dst *string) {
+
+	values := ctx.Value(Key0).(struct {
+		id   int
+		seed int64
+		size int
+	})
+
+	var (
+		id   = values.id
+		seed = values.seed
+		size = values.size
+	)
+
+	done := ctx.Done()
+
+	log.Printf("load generator created with id %d", id)
+
+	c, response, err := websocket.DefaultDialer.Dial(*dst, nil)
+	if err != nil {
+		log.Fatal(color.RedString("lg %d unable to open WebSocket connection: %s", id, err.Error()))
+	}
+	log.Printf("lg %d got reponse %s", id, response.Status)
+
+	s := rand.NewSource(seed)
+	r := rand.New(s)
+
+	for {
+		select {
+		case <-t:
+			{
+				msg := GenerateMessage(r, size)
+				err := c.WriteMessage(websocket.TextMessage, msg)
+				if err != nil {
+					log.Print(color.YellowString("lg %d unable to send message: %s", id, err.Error()))
+					//return
+				}
+			}
+		case <-done:
+			{
+				log.Print(color.YellowString("lg %d context cancelled", id))
+				goto TheEnd
+			}
+		}
+	}
+
+TheEnd:
+	err = c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if err != nil {
+		log.Print(color.RedString("lg %d error while trying to send close message: %s", id, err.Error()))
+	}
+	log.Print(color.GreenString("lg %d done", id))
+}
+
+func GenerateMessage(r *rand.Rand, size int) Message {
+	msg := make(Message, size)
+	for i := 0; i < size; i++ {
+		msg[i] = byte(r.Intn(z+1-a) + a)
+	}
+	return msg
+}
